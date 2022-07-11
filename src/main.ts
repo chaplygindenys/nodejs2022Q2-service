e9f0940fcd0454c6a58706fc6dc3f893a646486b
chaplygindenys <chaplygindenys@gmail.com>
2022-07-11 22:08:40 +0300

    strukture  and interface

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import PromiseOpenAPIObject from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const openAPIObject = await PromiseOpenAPIObject;
+  SwaggerModule.setup('doc', app, openAPIObject);
+
   await app.listen(4000);
 }
 bootstrap();
---
+++ src/tracks/tracks/tracks.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TracksService {
+  getTracks(): Track[] {
+    return;
+  }
+}
