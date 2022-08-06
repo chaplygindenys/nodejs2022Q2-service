592057c7bf080943bcd90a45ca50d384cce489bc
chaplygindenys <chaplygindenys@gmail.com>
2022-08-06 16:02:04 +0300

    passReqToCallback

--- src/auth/strategies/refTok.strategy.ts
+++ src/auth/strategies/refTok.strategy.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 type JwtPayload = {
   id: string;
   login: string;
@@ -12,13 +12,16 @@ export class RefTokStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: true,
+      passReqToCallback: true,
       secretOrKey: process.env.JWT_SECRET_REFRESH_KEY,
     });
   }
 
   validate(req: Request, payload: JwtPayload) {
     const refreshToken = req.get('autorithation').replace('Bearer', '').trim();
+
+    if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+
     return { ...payload, refreshToken };
   }
 }
